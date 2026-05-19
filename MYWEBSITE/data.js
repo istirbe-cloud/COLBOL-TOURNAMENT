063ead025f08aaa9a7550251c2a5695640cfb2b2
istirbe-cloud <istirbe@educacion.navarra.es>
2026-05-20 00:24:38 +0200

    Add files via upload

---
+++ MYWEBSITE/data.js
@@ -0,0 +1,10 @@
+// Paste your backup JSON structure inside the brackets below:
+const DEFAULT_TOURNAMENT_DATA = {
+  "teams": [
+    { "name": "Team Alpha", "img": "", "members": ["Player 1", "Player 2"], "w": 0, "d": 0, "l": 0, "pts": 0, "pj": 0 }
+  ],
+  "matches": [],
+  "scheduledMatches": {},
+  "interviews": [],
+  "knockoutMatches": { "semi1": null, "semi2": null, "final": null }
+};
